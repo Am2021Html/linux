c318ac0b681213696144bcfbab289794b348b652
amirhosein shahrstani <93426971+Am2021Html@users.noreply.github.com>
2022-03-17 10:58:18 +0330

    Create سال نو شمسی.py

---
+++ سال نو شمسی.py
@@ -0,0 +1,25 @@
+dAS = "Hello How are you?"
+BBC = ""
+BBa = ""
+Gasge = "***Happy New Year 1401 (shamsi)***"
+AM = "****************************"
+Mas = "Source The app in the My Github: (http://www.github.com/Am2021Html)"
+Had = "This is the app To Windows."
+err = "CopyRight: 2022 march - Founder: Amirhossein shahrestani. this is the App written With Python 3.0"
+ASa = "^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+por = "last update: 10:50 AM march 2022"
+PPa = "(**********************************)"
+Three = "Version: 2.04.1"
+print(dAS)
+print(BBC)
+print(BBa)
+print(Gasge)
+print(AM)
+print(Mas)
+print(Had)
+print(err)
+print(ASa)
+print(por)
+print(PPa)
+print(Three)
+# این کامنت رو در کدنویسی حذف کنید. ولی توجه کنید که این سال شمسی رو تبریک میگویید
